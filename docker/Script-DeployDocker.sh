93efc0cf2632d727176772f2c0f32ac2845b975b
Víctor M. Jiménez Hernando <68387861+VictorMjh@users.noreply.github.com>
2025-09-23 10:55:58 +0200

    Update Script-DeployDocker.sh

--- docker/Script-DeployDocker.sh
+++ docker/Script-DeployDocker.sh
@@ -2,11 +2,6 @@
 
 set -e  # Detener si hay errores
 
-# === Cargar variables desde el archivo .env ===
-set -o allexport
-source .env
-set +o allexport
-
 # === Actualizar el sistema ===
 sudo apt update && sudo apt upgrade -y
 
@@ -18,22 +13,10 @@ if ! command -v docker &> /dev/null; then
   sudo systemctl enable --now docker
 fi
 
-# === Opcional - Crear las red Docker externa ===
-#if ! docker network ls | grep -q "$NETWORK_NAME"; then
-#  echo "🌐 Creando red Docker externa: $NETWORK_NAME"
-#  docker network create "$NETWORK_NAME"
-#else
-#  echo "✅ Red Docker '$NETWORK_NAME' ya existe."
-#fi
-
 # === Asegurarse de que Docker esté en funcionamiento ===
 sudo systemctl start docker
 sudo systemctl enable docker
 echo "✅ Docker instalado y configurado. Listo para composer."
-
-# === Opcional desplegar con composer - comentado ===
-#echo "🚀 Desplegando servicios con Docker Compose..."
-#docker-compose -f "$COMPOSE_FILE" up -d --build
 
 # === Opcional Ejecutar contenedor de ejemplo (puedes personalizar esto) ===
 # sudo docker run -d --name mi_contenedor -p 80:80 nginx
